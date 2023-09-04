4c17a5b1fa3425b80f2671dd8be0b36e9d053d19
SichangiFelix <sichangifelix23@gmail.com>
2023-09-04 08:26:27 +0300

    Class for the app colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  Color scaffoldColor = const Color(0xffF9F9F9);
+  Color brown = const Color(0xffA0855B);
+  Color blue = const Color(0xffE1F4F3);
+  Color borderBlue = const Color(0xffC2F0FC);
+}
