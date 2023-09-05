d6d61849c232a75a8dcd218f777409a5053dd6d6
SichangiFelix <sichangifelix23@gmail.com>
2023-09-05 12:25:30 +0300

    Intergrated the sections pages to home screen

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/data/projects_data.dart';
+import 'package:portfolio/features/home/projects_page.dart';
+import 'package:portfolio/features/home/skills_page.dart';
 
 import '../../constants/colors.dart';
+import '../../data/skills_data.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController = PageController();
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,152 +24,204 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: AppColors.scaffoldColor,
-      body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        SizedBox(
-          width: screenWidth,
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(
-            vertical: screenHeight * 0.02,
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          SizedBox(
+            width: screenWidth,
           ),
-          height: screenHeight * 0.96,
-          width: screenWidth * 0.97,
-          padding: EdgeInsets.symmetric(
-              vertical: screenHeight * 0.02, horizontal: screenWidth * 0.03),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: AppColors.blue,
-              border: Border.all(
-                color: AppColors.borderBlue,
-                width: 2,
-              )),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "SICHANGI",
-                    style: TextStyle(
-                      color: AppColors.brown,
-                      fontSize: 24,
-                      fontFamily: "JuliusSansOne",
+          Container(
+            margin: EdgeInsets.symmetric(
+              vertical: screenHeight * 0.02,
+            ),
+            height: screenHeight * 0.96,
+            width: screenWidth * 0.97,
+            padding: EdgeInsets.symmetric(
+                vertical: screenHeight * 0.02, horizontal: screenWidth * 0.03),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: AppColors.blue,
+                border: Border.all(
+                  color: AppColors.borderBlue,
+                  width: 2,
+                )),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "SICHANGI",
+                      style: TextStyle(
+                        color: AppColors.brown,
+                        fontSize: 24,
+                        fontFamily: "JuliusSansOne",
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: screenWidth * 0.3,
-                  ),
-                  const Text(
-                    "Home",
-                    style: kHeaderStyle,
-                  ),
-                  const Text(
-                    "Skills",
-                    style: kHeaderStyle,
-                  ),
-                  const Text(
-                    "Experience",
-                    style: kHeaderStyle,
-                  ),
-                  const Text(
-                    "Projects",
-                    style: kHeaderStyle,
-                  ),
-                  const Text(
-                    "Contacts",
-                    style: kHeaderStyle,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      width: 2,
-                      color: AppColors.brown,
-                    )),
-                    child: const Text(
-                      "Reach out",
+                    SizedBox(
+                      width: screenWidth * 0.3,
+                    ),
+                    const Text(
+                      "Home",
                       style: kHeaderStyle,
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Hi! I am \nFelix Sichangi",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 32,
-                          fontFamily: "Lalezar",
-                        ),
+                    const Text(
+                      "Skills",
+                      style: kHeaderStyle,
+                    ),
+                    const Text(
+                      "Experience",
+                      style: kHeaderStyle,
+                    ),
+                    const Text(
+                      "Projects",
+                      style: kHeaderStyle,
+                    ),
+                    const Text(
+                      "Contacts",
+                      style: kHeaderStyle,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        width: 2,
+                        color: AppColors.brown,
+                      )),
+                      child: const Text(
+                        "Reach out",
+                        style: kHeaderStyle,
                       ),
-                      SizedBox(
-                        height: screenHeight * 0.07,
-                      ),
-                      const Text(
-                        "I am a software engineer building applications using the flutter framework \n\nI am a certified Oracle Cloud Infrastructure Associate. \n\nI am learning backend engineering, cloud computing and DevOps \n\nI love cars, motorsports, rugby, photography and travelling",
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Stack(
-                    children: [
-                      Container(
-                        height: screenHeight * 0.32,
-                        width: screenWidth * 0.3,
-                        padding: EdgeInsets.symmetric(
-                            vertical: screenHeight * 0.02,
-                            horizontal: screenWidth * 0.02),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(screenWidth * 0.02),
-                              bottomRight: Radius.circular(screenWidth * 0.02)),
-                          gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            stops: [0.1, 1.0],
-                            colors: [
-                              AppColors.brown,
-                              AppColors.blue,
-                            ],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Hi! I am \nFelix Sichangi",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 32,
+                            fontFamily: "Lalezar",
                           ),
-                          color: AppColors.brown,
                         ),
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        child: SizedBox(
-                            height: screenHeight * 0.32,
-                            width: screenWidth * 0.3,
-                            child: Image.asset(
-                              "assets/images/profile.png",
-                              height: screenHeight * 0.3,
-                              width: screenWidth * 0.4,
-                            )),
-                      ),
-                        
-                    
+                        SizedBox(
+                          height: screenHeight * 0.07,
+                        ),
+                        const Text(
+                          "I am a software engineer building applications using the flutter framework \n\nI am a certified Oracle Cloud Infrastructure Associate. \n\nI am learning backend engineering, cloud computing and DevOps \n\nI love cars, motorsports, rugby, photography and travelling",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Stack(
+                      children: [
+                        Container(
+                          height: screenHeight * 0.32,
+                          width: screenWidth * 0.3,
+                          padding: EdgeInsets.symmetric(
+                              vertical: screenHeight * 0.02,
+                              horizontal: screenWidth * 0.02),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(screenWidth * 0.02),
+                                bottomRight:
+                                    Radius.circular(screenWidth * 0.02)),
+                            gradient: LinearGradient(
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              stops: const [0.1, 1.0],
+                              colors: [
+                                AppColors.brown,
+                                AppColors.blue,
+                              ],
+                            ),
+                            color: AppColors.brown,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: SizedBox(
+                              height: screenHeight * 0.32,
+                              width: screenWidth * 0.3,
+                              child: Image.asset(
+                                "assets/images/profile.png",
+                                height: screenHeight * 0.3,
+                                width: screenWidth * 0.4,
+                              )),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: screenHeight * 0.05,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: screenHeight,
+            width: screenWidth,
+            child: DefaultTabController(
+              length: 4,
+              child: Column(
+                children: [
+                  TabBar(
+                    labelStyle: const TextStyle(
+                      fontFamily: "Lalezar",
+                      fontSize: 22,
+                    ),
+                    tabs: const [
+                      Tab(text: 'Skills'),
+                      Tab(text: 'Projects'),
+                      Tab(text: 'Certifications'),
+                      Tab(text: 'Achievements'),
                     ],
+                    onTap: (value) {
+                      setState(() {
+                        _currentPage = value;
+                      });
+                    },
+                    indicatorColor: AppColors.brown,
+                    labelColor: AppColors.brown,
+                    unselectedLabelColor: Colors.black,
+                    indicatorSize: TabBarIndicatorSize.tab,
+                  ),
+                  Expanded(
+                    child: TabBarView(
+                      children: [
+                        SkillsPage(
+                            screenHeight: screenHeight,
+                            screenWidth: screenWidth),
+                        ProjectsPage(screenWidth: screenWidth),
+                        Container(
+                          color: Colors.blue,
+                          child: const Center(child: Text('Page 3')),
+                        ),
+                        Container(
+                          color: Colors.orange,
+                          child: const Center(child: Text('Page 4')),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-              SizedBox(
-                height: screenHeight * 0.05,
-              ),
-            ],
+            ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
+
 
 const kHeaderStyle = TextStyle(
   color: Colors.black,
