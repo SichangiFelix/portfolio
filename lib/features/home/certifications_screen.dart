7f7201247ac6446f1cf6d9011641d321d3384864
SichangiFelix <sichangifelix23@gmail.com>
2023-09-05 13:33:49 +0300

    Built out the certification page

---
+++ lib/features/home/certifications_screen.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/colors.dart';
+
+class CertificationsPage extends StatelessWidget {
+  const CertificationsPage({
+    super.key,
+    required this.screenHeight,
+    required this.screenWidth,
+  });
+
+  final double screenHeight;
+  final double screenWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                height: screenHeight * 0.3,
+                width: screenWidth * 0.3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: AppColors.blue,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2), // Shadow color
+                      spreadRadius: 2, // Spread radius
+                      blurRadius: 10, // Blur radius
+                      offset:
+                          const Offset(2, 4), // Offset in the X, Y direction
+                    ),
+                  ],
+                ),
+                child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("OCI Foundations Associate"),
+                          Text("Oracle"),
+                        ],
+                      ),
+                      Text(
+                          "This is to certify that I am an Oracle cloud infrastructure foundations associate"),
+                      Text("August 2023 - August 2025"),
+                    ]),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                height: screenHeight * 0.3,
+                width: screenWidth * 0.3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: AppColors.blue,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2), // Shadow color
+                      spreadRadius: 2, // Spread radius
+                      blurRadius: 10, // Blur radius
+                      offset:
+                          const Offset(2, 4), // Offset in the X, Y direction
+                    ),
+                  ],
+                ),
+                child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("OCI Foundations Associate"),
+                          Text("Oracle"),
+                        ],
+                      ),
+                      Text(
+                          "This is to certify that I am an Oracle cloud infrastructure foundations associate"),
+                      Text("August 2023 - August 2025"),
+                    ]),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                height: screenHeight * 0.3,
+                width: screenWidth * 0.3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: AppColors.blue,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2), // Shadow color
+                      spreadRadius: 2, // Spread radius
+                      blurRadius: 10, // Blur radius
+                      offset:
+                          const Offset(2, 4), // Offset in the X, Y direction
+                    ),
+                  ],
+                ),
+                child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("OCI Foundations Associate"),
+                          Text("Oracle"),
+                        ],
+                      ),
+                      Text(
+                          "This is to certify that I am an Oracle cloud infrastructure foundations associate"),
+                      Text("August 2023 - August 2025"),
+                    ]),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                height: screenHeight * 0.3,
+                width: screenWidth * 0.3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: AppColors.blue,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2), // Shadow color
+                      spreadRadius: 2, // Spread radius
+                      blurRadius: 10, // Blur radius
+                      offset:
+                          const Offset(2, 4), // Offset in the X, Y direction
+                    ),
+                  ],
+                ),
+                child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("OCI Foundations Associate"),
+                          Text("Oracle"),
+                        ],
+                      ),
+                      Text(
+                          "This is to certify that I am an Oracle cloud infrastructure foundations associate"),
+                      Text("August 2023 - August 2025"),
+                    ]),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
