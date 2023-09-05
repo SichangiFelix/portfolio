db8cbfc6ecb43eb75a05a9608c8381a9d1676c59
SichangiFelix <sichangifelix23@gmail.com>
2023-09-05 12:25:09 +0300

    Built out the projects page

---
+++ lib/features/home/projects_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/colors.dart';
+import '../../data/projects_data.dart';
+
+class ProjectsPage extends StatelessWidget {
+  const ProjectsPage({
+    super.key,
+    required this.screenWidth,
+  });
+
+  final double screenWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+          itemCount: Projects().projects.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: screenWidth * 0.015,
+                  vertical: screenWidth * 0.01),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              width: screenWidth * 0.3,
+              decoration: BoxDecoration(
+                color: AppColors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    Projects().projects[index].title,
+                    style: TextStyle(
+                      color: AppColors.brown,
+                    ),
+                  ),
+                  Text(Projects().projects[index].description),
+                  Text(
+                    "Technologies used",
+                    style: TextStyle(
+                      color: AppColors.brown,
+                    ),
+                  ),
+                  Text(Projects().projects[index].techStack),
+                  Text(
+                    "Skills gained",
+                    style: TextStyle(
+                      color: AppColors.brown,
+                    ),
+                  ),
+                  Text(Projects().projects[index].skillsGained),
+                ],
+              ),
+            );
+          }),
+    );
+  }
+}
