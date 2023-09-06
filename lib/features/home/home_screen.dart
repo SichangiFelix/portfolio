9992b1ae3c73c5e476bbbf0b0040a1e18306ac42
SichangiFelix <sichangifelix23@gmail.com>
2023-09-06 23:53:58 +0300

    Built out rem sections

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -222,7 +222,7 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 Padding(
                   padding: EdgeInsets.only(left: screenWidth * 0.02),
-                  child: Text(
+                  child: const Text(
                     "Work Experience",
                     style: TextStyle(
                       fontFamily: "Lalezar",
@@ -233,19 +233,19 @@ class _HomeScreenState extends State<HomeScreen> {
                 Expanded(
                   child: ListView.builder(
                       scrollDirection: Axis.horizontal,
-                      itemCount: 3,
+                      itemCount: 6,
                       itemBuilder: (context, index) {
                         return Container(
                           margin: EdgeInsets.symmetric(
                               horizontal: screenWidth * 0.02,
                               vertical: screenHeight * 0.03),
-                          padding:  EdgeInsets.symmetric(
+                          padding: EdgeInsets.symmetric(
                             vertical: 10,
                             horizontal: screenWidth * 0.015,
                           ),
                           width: screenWidth * 0.3,
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
+                            borderRadius: BorderRadius.circular(5),
                             color: AppColors.blue,
                             boxShadow: [
                               BoxShadow(
@@ -258,17 +258,17 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                             ],
                           ),
-                          child:  Column(
+                          child: Column(
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
+                                const Text(
                                   "Jumuisha Limited",
                                   style: TextStyle(
                                     fontSize: 16,
                                   ),
                                 ),
-                                Text(
+                                const Text(
                                   "Software developer intern",
                                   style: TextStyle(
                                     fontSize: 14,
@@ -284,6 +284,260 @@ class _HomeScreenState extends State<HomeScreen> {
                               ]),
                         );
                       }),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: screenWidth,
+            height: screenHeight * 0.65,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: screenWidth * 0.02),
+                  child: const Text(
+                    "Education",
+                    style: TextStyle(
+                      fontFamily: "Lalezar",
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: screenWidth * 0.02,
+                              vertical: screenHeight * 0.03),
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenHeight * 0.015,
+                            horizontal: screenWidth * 0.015,
+                          ),
+                          width: screenWidth * 0.22,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: AppColors.lightBrown,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black
+                                    .withOpacity(0.2), // Shadow color
+                                spreadRadius: 2, // Spread radius
+                                blurRadius: 10, // Blur radius
+                                offset: const Offset(
+                                    2, 4), // Offset in the X, Y direction
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      "assets/images/star.png",
+                                      width: 80,
+                                    ),
+                                    const Text(
+                                      "Kenyatta University",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const Expanded(child: SizedBox.shrink()),
+                                const Text(
+                                  "Bsc. Computer Science",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "GPA - 66.0",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: AppColors.brown,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "Second class honors - upper division",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: AppColors.brown,
+                                  ),
+                                ),
+                                const Expanded(child: SizedBox.shrink()),
+                                Text(
+                                  "August 2019 - July 2024",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: AppColors.brown,
+                                  ),
+                                ),
+                              ]),
+                        );
+                      }),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: screenWidth,
+            height: screenHeight * 0.4,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: screenWidth * 0.02),
+                  child: const Text(
+                    "Referees",
+                    style: TextStyle(
+                      fontFamily: "Lalezar",
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: screenWidth * 0.02,
+                              vertical: screenHeight * 0.03),
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenHeight * 0.015,
+                            horizontal: screenWidth * 0.015,
+                          ),
+                          width: screenWidth * 0.2,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  "assets/images/profile_placeholder.png",
+                                  height: 60,
+                                ),
+                                const Text(
+                                  "Bill Jux",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                const Text(
+                                  "Software engineer",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                const Text(
+                                  "2447213324235",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ]),
+                        );
+                      }),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: screenWidth,
+            height: screenHeight * 0.4,
+            child: Stack(
+              children: [
+                Container(
+                  width: screenWidth,
+                  height: screenHeight * 0.4,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      stops: [0.7, 1],
+                      colors: [
+                        AppColors.blue,
+                        AppColors.scaffoldColor,
+                      ],
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Expanded(child: SizedBox.shrink()),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Contact me",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "+25473243959843",
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                            Text(
+                              "sichangi24@gmail.com",
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: screenWidth * 0.2,),
+                        Column(
+                          children: [
+                            Text(
+                              "Lets connect",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  "assets/images/linkedin.png",
+                                  width: 28,
+                                ),
+                                SizedBox(width: 8,),
+                                Image.asset("assets/images/twitter.png", width: 28),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Image.asset("assets/images/github.png", width: 28),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Expanded(child: SizedBox.shrink()),
+                  ],
                 ),
               ],
             ),
