8185a4ba7463bd32aa9be28a0ec9006e79b23961
SichangiFelix <sichangifelix23@gmail.com>
2023-09-07 22:08:13 +0300

    Redesigned the grid

--- lib/features/home/skills_page.dart
+++ lib/features/home/skills_page.dart
@@ -18,21 +18,24 @@ class SkillsPage extends StatelessWidget {
     return Container(
       child: GridView.builder(
         itemCount: 20,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisSpacing: screenWidth * 0.07,
+          mainAxisSpacing: screenHeight * 0.1,
+            childAspectRatio: 1.5, crossAxisCount: 5,),
         itemBuilder: (context, index) {
           return Container(
-            margin: EdgeInsets.symmetric(
-                vertical: screenHeight * 0.08, horizontal: screenWidth * 0.03),
+            // margin: EdgeInsets.symmetric(
+            //     vertical: screenHeight * 0.08, horizontal: screenWidth * 0.03),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
+              borderRadius: BorderRadius.circular(5),
               color:
                   (index % 2) == 0 ? AppColors.lightBrown : AppColors.lightBlue,
             ),
             child: Center(
               child: Text(
                 skills[index],
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 18,
                 ),
               ),
