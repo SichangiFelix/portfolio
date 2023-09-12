d75f309bb56e0e003a2620f0c6fb3dc300bdcd36
SichangiFelix <sichangifelix23@gmail.com>
2023-09-12 23:47:03 +0300

    Updated certifications

--- lib/features/home/certifications_screen.dart
+++ lib/features/home/certifications_screen.dart
@@ -84,94 +84,94 @@ class CertificationsPage extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text("OCI Foundations Associate"),
-                          Text("Oracle"),
+                          Text("Flutter development bootcamp"),
+                          Text("Udemy"),
                         ],
                       ),
                       Text(
-                          "This is to certify that I am an Oracle cloud infrastructure foundations associate"),
+                          "This is to certify that I completed the Flutter development bootcamp by Doctor Angela Yu"),
                       Text("August 2023 - August 2025"),
                     ]),
               ),
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 10,
-                ),
-                height: screenHeight * 0.3,
-                width: screenWidth * 0.3,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: AppColors.blue,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2), // Shadow color
-                      spreadRadius: 2, // Spread radius
-                      blurRadius: 10, // Blur radius
-                      offset:
-                          const Offset(2, 4), // Offset in the X, Y direction
-                    ),
-                  ],
-                ),
-                child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("OCI Foundations Associate"),
-                          Text("Oracle"),
-                        ],
-                      ),
-                      Text(
-                          "This is to certify that I am an Oracle cloud infrastructure foundations associate"),
-                      Text("August 2023 - August 2025"),
-                    ]),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 10,
-                ),
-                height: screenHeight * 0.3,
-                width: screenWidth * 0.3,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: AppColors.blue,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2), // Shadow color
-                      spreadRadius: 2, // Spread radius
-                      blurRadius: 10, // Blur radius
-                      offset:
-                          const Offset(2, 4), // Offset in the X, Y direction
-                    ),
-                  ],
-                ),
-                child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("OCI Foundations Associate"),
-                          Text("Oracle"),
-                        ],
-                      ),
-                      Text(
-                          "This is to certify that I am an Oracle cloud infrastructure foundations associate"),
-                      Text("August 2023 - August 2025"),
-                    ]),
-              ),
-            ],
-          ),
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //   children: [
+          //     Container(
+          //       padding: const EdgeInsets.symmetric(
+          //         vertical: 10,
+          //         horizontal: 10,
+          //       ),
+          //       height: screenHeight * 0.3,
+          //       width: screenWidth * 0.3,
+          //       decoration: BoxDecoration(
+          //         borderRadius: BorderRadius.circular(20),
+          //         color: AppColors.blue,
+          //         boxShadow: [
+          //           BoxShadow(
+          //             color: Colors.black.withOpacity(0.2), // Shadow color
+          //             spreadRadius: 2, // Spread radius
+          //             blurRadius: 10, // Blur radius
+          //             offset:
+          //                 const Offset(2, 4), // Offset in the X, Y direction
+          //           ),
+          //         ],
+          //       ),
+          //       child: const Column(
+          //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           children: [
+          //             Row(
+          //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //               children: [
+          //                 Text("OCI Foundations Associate"),
+          //                 Text("Oracle"),
+          //               ],
+          //             ),
+          //             Text(
+          //                 "This is to certify that I am an Oracle cloud infrastructure foundations associate"),
+          //             Text("August 2023 - August 2025"),
+          //           ]),
+          //     ),
+          //     Container(
+          //       padding: const EdgeInsets.symmetric(
+          //         vertical: 10,
+          //         horizontal: 10,
+          //       ),
+          //       height: screenHeight * 0.3,
+          //       width: screenWidth * 0.3,
+          //       decoration: BoxDecoration(
+          //         borderRadius: BorderRadius.circular(20),
+          //         color: AppColors.blue,
+          //         boxShadow: [
+          //           BoxShadow(
+          //             color: Colors.black.withOpacity(0.2), // Shadow color
+          //             spreadRadius: 2, // Spread radius
+          //             blurRadius: 10, // Blur radius
+          //             offset:
+          //                 const Offset(2, 4), // Offset in the X, Y direction
+          //           ),
+          //         ],
+          //       ),
+          //       child: const Column(
+          //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           children: [
+          //             Row(
+          //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //               children: [
+          //                 Text("OCI Foundations Associate"),
+          //                 Text("Oracle"),
+          //               ],
+          //             ),
+          //             Text(
+          //                 "This is to certify that I am an Oracle cloud infrastructure foundations associate"),
+          //             Text("August 2023 - August 2025"),
+          //           ]),
+          //     ),
+          //   ],
+          // ),
         ],
       ),
     );
